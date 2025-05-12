3c921bb09ab9eea82bcabe74725d7d473e497434
Frank Pier <frank.pier@rwth-aachen.de>
2025-05-12 22:09:51 +0200

    Improved foundations

--- buildings/base_foundation.py
+++ buildings/base_foundation.py
@@ -1,12 +1,15 @@
 from gdpc import Editor, Block
 from gdpc.geometry import placeCuboid
-
+from gdpc.vector_tools import Rect
+import numpy as np
 from typing import Sequence, Union
 
-def clearAreaAbove(editor: Editor, length: int, width: int, height: int):
-    placeCuboid(editor, (0, 1, 0), (length, height, width), Block("air"))
-
-def placeRectFoundation(editor: Editor, length: int, width: int,
-                        block: Union[Block, Sequence[Block]]):
-    placeCuboid(editor, (0, 0, 0), (length, -5, width), block)
-    clearAreaAbove(editor, length, width, 10)
+def placeRectFoundation(editor: Editor, area: Rect,
+                        block: Union[Block, Sequence[Block]]) -> int:
+    world_slice = editor.loadWorldSlice(area)
+    height_map = world_slice.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
+    min_y = np.min(height_map)
+    print(min_y)
+    placeCuboid(editor, (area.offset.x, min_y-1, area.offset.y), (area.offset.x+area.size.x-1, min_y-3, area.offset.y+area.size.y-1), block)
+    placeCuboid(editor, (area.offset.x, min_y, area.offset.y), (area.offset.x+area.size.x-1, min_y+20, area.offset.y+area.size.y-1), Block("air"))
+    return min_y
