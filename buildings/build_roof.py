7c70880c9836b6cddae8ac544cd57a286ecef320
Frank Pier <frank.pier@rwth-aachen.de>
2025-06-17 14:21:24 +0200

    Added brick roofs

--- buildings/build_roof.py
+++ buildings/build_roof.py
@@ -46,8 +46,14 @@ def build_wooden_roof(editor: Editor, tile_array: npt.NDArray[any], tile_size: i
         roof_rotation = 1
 
     target_box = Box(nw_corner_coordinate, (eastwest_length, width//2+1, northsouth_length))
+    roof_type = choice([0,1])
     with editor.pushTransform(rotatedBoxTransform(target_box, roof_rotation)):
-        generic_wooden_roof(editor, length, width, wood_type)
+        match roof_type:
+            case 0:
+                generic_wooden_roof(editor, length, width, wood_type)
+            case 1:
+                generic_brick_roof(editor, length, width, wood_type)
+        
      
 
 def generic_wooden_roof(editor: Editor, length: int, width: int, wood_type: str):
@@ -87,4 +93,42 @@ def generic_wooden_roof(editor: Editor, length: int, width: int, wood_type: str)
             placeCuboid(editor, (width//2, i+1, -1), (width//2, i+1, length-1+1), Block(slab_name))
             placeCuboid(editor, (width//2, 1, 0), (width//2, i, 0), Block(planks_name))
             placeCuboid(editor, (width//2, 1, length-1), (width//2, i, length-1), Block(planks_name))
+
+def generic_brick_roof(editor: Editor, length: int, width: int, wood_type: str):
+    stairs_name = f"{wood_type}_stairs"
+    planks_name = f"{wood_type}_planks"
+    slab_name = f"{wood_type}_slab"
+    log_name = f"{wood_type}_log"
+    
+    east_block = Block("brick_stairs", {"facing": "east"})
+    west_block = Block("brick_stairs", {"facing": "west"})
+
+    east_flipped_block = Block("brick_stairs", {"facing": "west", "half": "top"})
+    west_flipped_block = Block("brick_stairs", {"facing": "east", "half": "top"})
+
+    for i in range(width//2 + 1):
+        placeCuboid(editor, (-1+i, i, -1), (-1+i, i, length), east_block)
+        placeCuboid(editor, (width-i, i, -1), (width-i, i, length), west_block)
+        
+        if i < width//2:
+            if i % 2 == 1:
+                placeCuboid(editor, (i, 1, 0), (i,1+i,0), Block("white_concrete"))
+                placeCuboid(editor, (width-1-i, 1, 0), (width-1-i,1+i,0), Block("white_concrete"))
+                placeCuboid(editor, (i, 1, length-1), (i,1+i,length-1), Block("white_concrete"))
+                placeCuboid(editor, (width-1-i, 1, length-1), (width-1-i,1+i,length-1), Block("white_concrete"))
+            else:
+                placeCuboid(editor, (i, 1, 0), (i,1+i,0), Block(log_name))
+                placeCuboid(editor, (width-1-i, 1, 0), (width-1-i,1+i,0), Block(log_name))
+                placeCuboid(editor, (i, 1, length-1), (i,1+i,length-1), Block(log_name))
+                placeCuboid(editor, (width-1-i, 1, length-1), (width-1-i,1+i,length-1), Block(log_name))
+            
+            editor.placeBlock((i, i, -1), east_flipped_block)
+            editor.placeBlock((width-1-i, i, -1), west_flipped_block)
+            editor.placeBlock((i, i, length-1+1), east_flipped_block)
+            editor.placeBlock((width-1-i, i, length-1+1), west_flipped_block)
+        elif width % 2 == 1:
+            placeCuboid(editor, (width//2, i, -1), (width//2, i, length-1+1), Block("bricks"))
+            placeCuboid(editor, (width//2, i+1, -1), (width//2, i+1, length-1+1), Block("brick_slab"))
+            placeCuboid(editor, (width//2, 1, 0), (width//2, i, 0), Block("white_concrete"))
+            placeCuboid(editor, (width//2, 1, length-1), (width//2, i, length-1), Block("white_concrete"))
     
