200723f0e4af4fc80efdd0fdfc13687c37df14e1
Frank Pier <frank.pier@rwth-aachen.de>
2025-06-20 15:22:40 +0200

    added functions to find a lectern

--- buildings/narrative.py
+++ buildings/narrative.py
@@ -1,7 +1,7 @@
 from maps.blueprint import Blueprint
 from agents.plots import PlotType
 
-from gdpc.vector_tools import Box
+from gdpc.vector_tools import Rect
 from pyglm.glm import ivec3
 import numpy as np
 
@@ -30,5 +30,29 @@ def find_lecterns_in_plot_type(blueprint: Blueprint, plot_type: PlotType) -> lis
                 lectern_facing.append(block.states["facing"])
                 
     return lectern_coords, lectern_facing
+
+def find_lecterns_in_plot(blueprint: Blueprint, plot_area: Rect) -> list[ivec3]:
+
+    match blueprint.map[plot_area.offset.x, plot_area.offset.y]:
+        case 175:
+            possible_heights =  [1,6,11]
+        case 255:
+            possible_heights = [4]
+        case _:
+            return None
+    
+    editor = blueprint.map_features.editor
+    build_area = editor.getBuildArea()
+    lectern_coords = []
+    lectern_facing = []
+    for x,z in plot_area:
+        for y in possible_heights:
+            block = editor.getBlockGlobal((build_area.offset.x + x, blueprint.plot_heights[x,z]+1+y, build_area.offset.z + z))
+            if block.id == "minecraft:lectern":
+                lectern_coords.append(ivec3(build_area.offset.x + x,blueprint.plot_heights[x,z]+1+y,build_area.offset.z + z))
+                lectern_facing.append(block.states["facing"])
+                
+    return lectern_coords, lectern_facing
+
                 
 
